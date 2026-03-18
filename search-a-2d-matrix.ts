d59f76c9f9e32f68b2cd9c6128cee8ee7ecbab8e
Igor Doroshenko <oknehsorod@gmail.com>
2026-03-18 15:45:36 +0200

    Optimize "search-a-2d-matrix" solution

--- search-a-2d-matrix.ts
+++ search-a-2d-matrix.ts
@@ -1,38 +1,22 @@
-function search(nums: number[], target: number): number {
-  let l = 0,
-    r = nums.length - 1;
-
-  while (l <= r) {
-    let m = l + Math.floor((r - l) / 2);
-    if (nums[m] < target) {
-      l = m + 1;
-    } else if (nums[m] > target) {
-      r = m - 1;
-    } else {
-      return m;
-    }
-  }
-
-  return -1;
-}
-
 function searchMatrix(matrix: number[][], target: number): boolean {
+  const n = matrix[0].length;
   let l = 0,
-    r = matrix.length - 1;
+    r = matrix.length * n - 1;
 
   while (l <= r) {
     let m = l + Math.floor((r - l) / 2),
-      first = matrix[m][0],
-      last = matrix[m][matrix[m].length - 1];
+      v = matrix[Math.floor(m / n)][m % n];
 
-    if (last < target) {
+    if (v < target) {
       l = m + 1;
-    } else if (first > target) {
+    } else if (v > target) {
       r = m - 1;
     } else {
-      return search(matrix[m], target) > -1;
+      return true;
     }
   }
 
   return false;
 }
+
+searchMatrix([[1]], 2);
