8ed47717d8c8a4955998cac5ad1eb000e7d60c9e
Igor Doroshenko <oknehsorod@gmail.com>
2026-04-03 10:58:13 +0300

    Add "jump-game" solution (greedy)

---
+++ src/jump-game.ts
@@ -0,0 +1,19 @@
+function canJump(nums: number[]): boolean {
+  let goal = nums.length - 1;
+
+  for (let i = goal - 1; i >= 0; i -= 1) {
+    if (i + nums[i] >= goal) {
+      goal = i;
+    }
+  }
+
+  return goal === 0;
+}
+
+canJump([2, 3, 1, 5, 4]);
+
+/*
+
+[2, 3, 1, |1|, 4]
+
+*/
