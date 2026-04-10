98b58c93c37a81b380134d853b7fef078815fa37
Igor Doroshenko <oknehsorod@gmail.com>
2026-04-10 14:52:51 +0300

    Add "house-robber" solution (dp/bottom-up/optimized)

---
+++ src/house-robber.ts
@@ -0,0 +1,13 @@
+function rob(nums: number[]): number {
+  const dp = new Array(nums.length + 1);
+  dp[nums.length] = 0;
+  dp[nums.length - 1] = nums.at(-1);
+
+  for (let i = nums.length - 2; i >= 0; i -= 1) {
+    dp[i] = Math.max(nums[i] + dp[i + 2], dp[i + 1]);
+  }
+
+  return Math.max(dp[0], dp[1]);
+}
+
+rob([1, 2, 3, 1]);
