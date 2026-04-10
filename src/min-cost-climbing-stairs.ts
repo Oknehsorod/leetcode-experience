57b73d3c69cb7c7a4ac1ad885887883d6ce842fe
Igor Doroshenko <oknehsorod@gmail.com>
2026-04-10 14:37:46 +0300

    Add "min-cost-climbing-stairs" solution (dp/bottom-up/optimized)

---
+++ src/min-cost-climbing-stairs.ts
@@ -0,0 +1,16 @@
+function minCostClimbingStairs(cost: number[]): number {
+  if (cost.length <= 2) return Math.min(...cost);
+
+  const dp = new Array(cost.length);
+  dp[cost.length] = 0;
+  dp[cost.length - 1] = cost.at(-1);
+
+  for (let i = cost.length - 2; i >= 0; i--) {
+    dp[i] = cost[i] + Math.min(dp[i + 1], dp[i + 2]);
+  }
+
+  return Math.min(dp[0], dp[1]);
+}
+
+//minCostClimbingStairs([10, 15, 20]);
+minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]);
