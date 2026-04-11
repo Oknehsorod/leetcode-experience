f666661aa4b13925aa93ab8417f0b1c86f6fcaec
Igor Doroshenko <oknehsorod@gmail.com>
2026-04-11 12:15:02 +0300

    Update the solution

--- src/house-robber-ii.ts
+++ src/house-robber-ii.ts
@@ -1,7 +1,7 @@
 function rob(nums: number[]): number {
   if (nums.length <= 2) return Math.max(...nums);
 
-  const r = (ns: number[]) => {
+  const helper = (ns: number[]) => {
     const dp = new Array(ns.length + 1);
     dp[ns.length] = 0;
     dp[ns.length - 1] = ns.at(-1);
@@ -13,7 +13,7 @@ function rob(nums: number[]): number {
     return dp[0];
   };
 
-  return Math.max(r(nums.slice(0, -1)), r(nums.slice(1)));
+  return Math.max(helper(nums.slice(0, -1)), helper(nums.slice(1)));
 }
 
 console.log(rob([3, 2]), rob([2, 3]));
